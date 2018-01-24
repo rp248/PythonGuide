59b39dbd010b02c83702f11df684a7eb5d013681
rajesh <rajeshp248@gmail.com>
2018-01-24 16:05:12 +0530

    added notes for number.py

--- numbers.py
+++ numbers.py
@@ -19,6 +19,71 @@ _complex_number = 3+2j
 print(_complex_number)  # (3+2j)
 print(type(_complex_number))    # <class 'complex'>
 
+# not Operator
+print(not (200 == 210))  # True
+print(not ("Java" == "Java"))  # False
+
+# or Operator
+
+print((100 < 200) or (55 < 6))  # True
+print((100 < 99) or (55 < 6))  # True.
+'''
+In above case, (100>99) is True, and so is the whole logical expression. 
+Hence there is no need to evaluate the expression (55<6).
+'''
+# Truthy and Falsy values
+
+'''
+ Truthy Values : Values which are equivalent to 'True' is known as Truthy values.
+ Falsy Values: Values which are equivalent to 'False' is known as False values.
+ 
+ Falsy Values in Python:
+ None, False, 0, 0.0
+ Empty Sequence (List) for example, '', [], ()
+ Empty Dictionary for example, {}
+ 
+ We can check whether the value is Truthy or Falsy using bool(value_to_find) 
+ 
+'''
+print(bool(0))  # False
+print(bool(''))  # False
+print(bool([]))  # False
+print(bool({}))  # False
+
+# bool type
+
+'''
+A bool type represents only two state  true or false. In Python we define bool types using reserved keyword 'True' and 
+'False'
+Python uses 1 and 0 to represent true and false.We can verify this fact by using int() function on True and False 
+keywords. 
+'''
+
+print(int(True))  # 1
+print(int(False))  # 0
+
+# Breaking single statements into multiple lines
+'''
+Python allows us to break long expression into multiple lines using line continuation symbol ( \ ). 
+The \ symbol tells the Python interpreter that the statement is continued on the next line. 
+For example:
+'''
+
+_long_variable = "Hello Hello Hello Hello Hello Hello Hello Hello Hello" \
+                 " Hello Hello Hello Hello Hello" \
+                 " Hello Hello Hello Hello Hello Hello Hello Hello "
+print(_long_variable)
+
+# Type conversion
+'''
+int(), float(),str() function can be used for type conversion
+'''
+print(int("2"))  # 2
+
+
+
+
+
 
 
 
